48e0f8d4e7502fb7e054308b7f1116f363201089
juan miranda <jpmiranda8183@gmail.com>
2024-11-17 22:12:30 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoProgramacionEv4.git/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoProgramacionEv4.git.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
